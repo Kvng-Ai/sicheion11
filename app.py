f6a4ff4d02d3f98ed8bdee66e62738d6ae7b0397
Kvng-Ai <21kevinflores@gmail.com>
2024-10-28 22:54:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key = 'your_secret_key'  # Replace with your secret key
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'hummelstella2@gmail.com '  # Your Gmail address
-app.config['MAIL_PASSWORD'] = 'pqlyygsxziipgjvq'   # Your Gmail password or app-specific password
+app.config['MAIL_USERNAME'] = '21kevinflores@gmail.com '  # Your Gmail address
+app.config['MAIL_PASSWORD'] = 'uhkwpsotruoborpo'   # Your Gmail password or app-specific password
 
 mail = Mail(app)
 
